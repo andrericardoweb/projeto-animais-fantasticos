763a544cb8470c00d87c5e5476ea583b83e39898
andrericardoweb <andrericardoweb@gmail.com>
2021-09-19 12:21:54 -0300

    add function initAnimacaoScroll

--- script.js
+++ script.js
@@ -57,6 +57,27 @@ function initScrollSuave() {
   });
 }
 
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScrool() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
+    }
+
+    window.addEventListener("scroll", animaScrool);
+
+    animaScrool();
+  }
+}
+
 initTabNav();
 initAccordion();
 initScrollSuave();
+initAnimacaoScroll();
